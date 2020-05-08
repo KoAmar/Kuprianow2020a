215c605c384e064929e4295bf9c536888b438ef4
KoAmar <43239620+KoAmar@users.noreply.github.com>
2020-05-08 09:49:57 +0300

    lab6🤔

---
+++ lab_6/Forms/Audit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_6.Forms
+{
+    public partial class Audit : Form
+    {
+        public Audit()
+        {
+            InitializeComponent();
+        }
+
+        private void auditLogBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.auditLogBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.hotelDBDataSet);
+
+        }
+
+        private void Audit_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'hotelDBDataSet.Rooms' table. You can move, or remove it, as needed.
+            this.roomsTableAdapter.Fill(this.hotelDBDataSet.Rooms);
+            // TODO: This line of code loads data into the 'hotelDBDataSet.Clients' table. You can move, or remove it, as needed.
+            this.clientsTableAdapter.Fill(this.hotelDBDataSet.Clients);
+            // TODO: This line of code loads data into the 'hotelDBDataSet.Administrators' table. You can move, or remove it, as needed.
+            this.administratorsTableAdapter.Fill(this.hotelDBDataSet.Administrators);
+            // TODO: This line of code loads data into the 'hotelDBDataSet.AuditLog' table. You can move, or remove it, as needed.
+            this.auditLogTableAdapter.Fill(this.hotelDBDataSet.AuditLog);
+
+        }
+    }
+}
---
+++ lab_6/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using lab_6.Forms;
+
+namespace lab_6
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void AuditBt_Click(object sender, EventArgs e)
+        {
+            var form = new Audit();
+            form.ShowDialog(this);
+        }
+
+        private void AdminsBt_Click(object sender, EventArgs e)
+        {
+            //var form = new AdminsForm();
+            //form.ShowDialog(this);
+        }
+
+        private void ClientsBt_Click(object sender, EventArgs e)
+        {
+            //var form = new Clients();
+            //form.ShowDialog(this);
+        }
+
+        private void RoomsBt_Click(object sender, EventArgs e)
+        {
+            //var form = new RoomsForm();
+            //form.ShowDialog(this);
+        }
+
+        private void LangBt_Click(object sender, EventArgs e)
+        {
+            //var form = new LangForm();
+            //form.ShowDialog(this);
+        }
+    }
+}
