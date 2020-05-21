eb2266312863bcde72a0ddf240b9050ea6bddec4
KoAmar <43239620+KoAmar@users.noreply.github.com>
2020-05-21 18:31:55 +0300

    lab8 start

--- lab_8/Properties/AssemblyInfo.cs
+++ lab_8/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("lab_7_2")]
+[assembly: AssemblyTitle("lab_8")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("lab_7_2")]
+[assembly: AssemblyProduct("lab_8")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- lab_8/Properties/Resources.Designer.cs
+++ lab_8/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace lab_7_2.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("lab_7_2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("lab_8.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
